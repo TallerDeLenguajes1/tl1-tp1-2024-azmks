4c1f4768fc721c4c04330cdaaf9e9e461a6c2d3d
Mikael <lescanostisman.mikael@gmail.com>
2024-03-26 13:17:07 -0300

    realizado el punto 3.e)

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,15 @@
 int main ()
 {
     printf("Hola mundo");
+
+    int a = 12;
+    int * punt_a = &a;
+
+    printf("\nEl contenido del puntero es: %d", *punt_a);
+    printf("\nLa direccion almacenada por el puntero es: %p", punt_a);
+    printf("\nLa direccion de memoria de la variable es: %p", &a);
+    printf("\nLa direccion de memoria del puntero es: %p", &punt_a);
+    printf("\nEl tamano de memoria utilizado por la variable es: %d bytes", sizeof(a));
+
     return 0;
 }
